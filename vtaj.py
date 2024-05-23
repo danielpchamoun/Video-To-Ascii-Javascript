9e663c11a351c1638acaa5e6ad267fa4cdbffebf
Daniel Chamoun <chamoun2@illinois.edu>
2024-05-23 03:41:30 -0500

    added time constraints, need to update some ui stuff tomorrow and add color overlay

--- vtaj.py
+++ vtaj.py
@@ -22,7 +22,7 @@ import numpy
 # button will be clicked in the main window
 userCustomColor="red"
 def choose_color():
- 
+    global userCustomColor
     # variable to store hexadecimal code of color
     color_code = colorchooser.askcolor(title ="Choose color") 
     userCustomColor = color_code[1] #do more splitting here
@@ -32,28 +32,63 @@ def choose_color():
 userVideoInput = ""
 def choose_file():
     file_path = tkinter.filedialog.askopenfilename(filetypes=(("Video files", "*.mp4;"),("All files", "*.*") ))
-    print(file_path)
-    userVideoInput = file_path.split('/')[-1]
+    print("Selected file path:", file_path)
+    
     selectedFileEntry.delete(0, END)
     selectedFileEntry.insert(0, file_path)
+    selectedFileEntry.config(fg='black')
+
     return
+
+
+def selectedFileEntryFocus(event):
+    if selectedFileEntry.get() == 'Enter file path or paste YouTube link here':
+        selectedFileEntry.delete(0, END)
+        selectedFileEntry.insert(0, '')
+        selectedFileEntry.config(fg='black')
+
+def selectedFileEntryUnfocus(event):
+    if selectedFileEntry.get() == '':
+        selectedFileEntry.insert(0, 'Enter file path or paste YouTube link here')
+        selectedFileEntry.config(fg='grey')
+
+def startEntryFocus(event):
+    if startEntry.get() == '00:00:00':
+        startEntry.delete(0, END)
+        startEntry.insert(0, '')
+        startEntry.config(fg='black')
+
+def startEntryUnfocus(event):
+    if startEntry.get() == '':
+        startEntry.insert(0, '00:00:00')
+        startEntry.config(fg='grey')
+
+
 
 def main():
 
-    youtubeLink = userVideoInput #might need to swap these two arguments, user should be able to simply add either youtube video or direct mp4 to convert to ascii
+    userVideoInput = selectedFileEntry.get()
+    print(userVideoInput)
+    if userVideoInput.startswith("https://"):
+        print("Downloading video from: " + userVideoInput)
+        try:
+            yt = YouTube(userVideoInput)
+            mp4 = yt.streams.get_highest_resolution()
+            mp4.download(filename="video.mp4")
+            inputfile = "video.mp4"
+            print("Video downloaded successfully.")
+        except Exception as e:
+            print("Error downloading video:", e)
+            return
+    else:
+        inputfile = userVideoInput
+    print(inputfile)
+    cap = cv.VideoCapture(inputfile)
+    if not cap.isOpened():
+        print("Error: Couldn't open video file.")
+        return
 
-    #get highest quality video from stream list
-
-    inputfile = userVideoInput
-
-    if inputfile.startswith("https://"): 
-        print("Downloading video from: "+youtubeLink)
-        yt = YouTube(youtubeLink)
-        mp4 = yt.streams.get_highest_resolution()
-        mp4.download(filename="video.mp4")
-
-    cap = cv.VideoCapture("video.mp4")
-
+    fps = cap.get(cv.CAP_PROP_FPS)
 
     # 70 levels of gray
     ascii_ramp = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
@@ -67,43 +102,58 @@ def main():
     asciiFrames = []
     windowW = 0
     windowH = 0
+
+    #time parameters
+    startTime = startEntry.get()
+    endTime = endEntry.get()
+
+    if startTime:
+        startSeconds = int(startTime[0:2]) * 3600 + int(startTime[3:5]) * 60 + int(startTime[6:8])
+    else:
+        startSeconds = 0
+
+    if endTime:
+        endSeconds = int(endTime[0:2]) * 3600 + int(endTime[3:5]) * 60 + int(endTime[6:8])
+    else:
+        endSeconds = int(cap.get(cv.CAP_PROP_FRAME_COUNT) / fps)
+
     while cap.isOpened():
         ret, frame = cap.read() #gets next frame
-        if ret:
-            resized = cv.resize(frame, None, fx=factorX, fy=factorY, interpolation=cv.INTER_CUBIC)
-
-        height = int(resized.shape[0])
-        width = int(resized.shape[1])
-        windowW = width
-        windowH = height
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
+        if not ret or count / fps >= endSeconds:
+            print("Done.")
             break
-        gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY) #also the same as intensity
-        colorValues = []
-        asciiValues = []
-        for y in range(height):
-            asciiRow = []
-            colorRow = []
-            for x in range(width):
-                color = resized[y,x].tolist()
-                ascii = ascii_ramp[int((gray[y,x]*69)/255)]
-                colorRow.append(color)
-                asciiRow.append(ascii)
-            colorValues.append(colorRow)
-            asciiValues.append(asciiRow)
-        asciiFrames.append(asciiValues)
-        colorFrames.append(colorValues)
-        print(str(count) + "/" +str(int(cap.get(cv.CAP_PROP_FRAME_COUNT))))
+        
+        if count / fps >= startSeconds:
+            resized = cv.resize(frame, None, fx=factorX, fy=factorY, interpolation=cv.INTER_CUBIC)
+            height = int(resized.shape[0])
+            width = int(resized.shape[1])
+            windowW = width
+            windowH = height
+
+            gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY) #also the same as intensity
+            colorValues = []
+            asciiValues = []
+            for y in range(height):
+                asciiRow = []
+                colorRow = []
+                for x in range(width):
+                    color = resized[y,x].tolist()
+                    ascii = ascii_ramp[int((gray[y,x]*69)/255)]
+                    colorRow.append(color)
+                    asciiRow.append(ascii)
+                colorValues.append(colorRow)
+                asciiValues.append(asciiRow)
+            asciiFrames.append(asciiValues)
+            colorFrames.append(colorValues)
+
+            #update with new time later
+            print("Converting:" + str(min(100, round((count / (fps * endSeconds)) * 100, 2)))+"%")
 
         count+=1
         cv.imshow('resized', gray)
         if cv.waitKey(1) == ord('q'):
             break
-        os.system('cls')
-    #print(asciiValues)
-    #print(colorValues)
+
     jsoutput = open("animation.js","a")
 
     jsoutput.write("colorValues ="+str(colorFrames)+";\n")
@@ -139,15 +189,19 @@ root.geometry("510x100")
 root.resizable(False, False)
 frm = ttk.Frame(root, padding=2.5)
 frm.grid()
-Label(frm, text="Paste youtube link below:").grid(column=0, row=0) #fix if needed
 
 
-ttk.Button(frm, text="Choose .mp4", command=choose_file).grid(column=1, row=1, sticky="w")
+ttk.Button(frm, text="Browse", command=choose_file).grid(column=1, row=1, sticky="w")
 selectedFileEntry = ttk.Entry(frm, width=55)
 selectedFileEntry.grid(column=0, row=1)
 
-startEntry = ttk.Entry(frm, width=10)
+startEntry = Entry(frm, width=10,fg='grey')
+startEntry.insert(0, '00:00:00')
+startEntry.bind('<FocusIn>', startEntryFocus)
+startEntry.bind('<FocusOut>', startEntryUnfocus)
+
 startEntry.grid(column=1, row=3,sticky="w")
+
 
 startLabel = Label(frm, text="Start")
 startLabel.grid(column=1, row=4 ,sticky="w")
@@ -167,5 +221,13 @@ colorFilterEnabled = BooleanVar()
 autoDownloadCheckbox = Checkbutton(frm, text="Enable Color Filter", variable=colorFilterEnabled)
 autoDownloadCheckbox.grid(column=0, row=4)
 ttk.Button(frm, text="Convert Video", command=main).grid(column=2, row=1,sticky="w")
+
+
+selectedFileEntry = Entry(frm, width=55, fg='grey')
+selectedFileEntry.insert(0, 'Enter file path or paste YouTube link here')
+selectedFileEntry.bind('<FocusIn>', selectedFileEntryFocus)
+selectedFileEntry.bind('<FocusOut>', selectedFileEntryUnfocus)
+selectedFileEntry.grid(column=0, row=1)
+
 root.mainloop()
 
